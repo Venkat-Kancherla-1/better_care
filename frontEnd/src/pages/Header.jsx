75ad1ac0db05f348b98f7d29ffedfbcb35e26d91
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-03-10 05:10:21 +0530

    update Header.jsx home route

--- frontEnd/src/pages/Header.jsx
+++ frontEnd/src/pages/Header.jsx
@@ -36,7 +36,7 @@ export default function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <NavLink
-                  to="/"
+                  to="/home"
                   className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
