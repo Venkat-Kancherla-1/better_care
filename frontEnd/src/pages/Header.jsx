d1a64d5aadacec1be42e6ad04c31738ffa90fee8
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-03-10 05:06:41 +0530

    Update Header, Footer and cARDS.JSX

--- frontEnd/src/pages/Header.jsx
+++ frontEnd/src/pages/Header.jsx
@@ -10,17 +10,23 @@ export default function Header() {
             <img src={logo} className="mr-3 h-12" alt="Logo" />
           </Link>
           <div className="flex items-center lg:order-2">
-            <Link
+            {/* <Link
               to="#"
               className="text-gray-800 hover:bg-gray-50 text-lg-transition:hover focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Log in
-            </Link>
-            <Link
+            </Link> */}
+            {/* <Link
               to="signup"
               className="text-white bg-[#8333ea] hover:bg-[#8333ea] hover:scale-1 focus:ring-2 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
               Get started
+            </Link> */}
+            <Link
+              to="/"
+              className="text-white bg-[#00dfc2] hover:bg-teal-500 hover:scale-1 focus:ring-2 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+            >
+              Logout
             </Link>
           </div>
           <div
@@ -35,7 +41,7 @@ export default function Header() {
                     `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
                                           isActive
-                                            ? "text-blue-500"
+                                            ? "text-[#00dfc2]"
                                             : "text-gray-700"
                                         } lg:hover:bg-transparent lg:border-0 hover:text-blue-500 lg:p-0`
                   }
@@ -50,9 +56,9 @@ export default function Header() {
                     `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
                                           isActive
-                                            ? "text-blue-500"
+                                            ? "text-[#00dfc2]"
                                             : "text-gray-700"
-                                        } lg:hover:bg-transparent lg:border-0 hover:text-blue-500 lg:p-0`
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-[#00dfc2] lg:p-0`
                   }
                 >
                   About
@@ -65,16 +71,16 @@ export default function Header() {
                     `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
                                         ${
                                           isActive
-                                            ? "text-blue-500"
+                                            ? "text-[#00dfc2]"
                                             : "text-gray-700"
-                                        } lg:hover:bg-transparent lg:border-0 hover:text-blue-500 lg:p-0`
+                                        } lg:hover:bg-transparent lg:border-0 hover:text-[#00dfc2] lg:p-0`
                   }
                 >
                   Contact
                 </NavLink>
               </li>
               <li></li>
-              <li>
+              {/* <li>
                 <NavLink
                   to="/user"
                   className={({ isActive }) =>
@@ -88,7 +94,7 @@ export default function Header() {
                 >
                   User
                 </NavLink>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
