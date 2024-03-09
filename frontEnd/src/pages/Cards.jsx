4593e1dcd0bc41e4df4ce7fcf12ae525b6a459f1
EccentricHarry <harinathrao13@gmail.com>
2024-03-10 03:33:10 +0530

    home_grid_final

--- frontEnd/src/pages/Cards.jsx
+++ frontEnd/src/pages/Cards.jsx
@@ -3,33 +3,64 @@ import {
   Card,
   CardHeader,
   CardBody,
-  CardFooter,
   Typography,
+  CardFooter
 } from "@material-tailwind/react";
+import { cards1, cards2 } from '../constants';
 
-export default function Cards() {
+const Cards = () => {
   return (
-    <Card className="mt-6 mx-auto max-w-lg md:max-w-2xl lg:max-w-4xl">
-      <CardHeader color="blue-gray" className="relative h-56 rounded-lg">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-          className="object-cover w-full h-full rounded-lg"
-        />
-      </CardHeader>
-      <CardBody className="py-6">
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to "Naviglio" where you can enjoy the main night life
-          in Barcelona.
-        </Typography>
-      </CardBody>
-      <CardFooter className="flex justify-center pt-0">
-        <button className="bg-black text-white px-4 py-2 rounded-lg">Learn More</button>
-      </CardFooter>
-    </Card>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-6">
+        {cards1.map((card, index) => (
+          <Card key={index} className="mt-6">
+            <CardHeader color="blue-gray" className="relative h-56 rounded-lg">
+              <img
+                src={card.imageURL}
+                alt="card-image"
+                className="object-cover w-full h-full rounded-lg"
+              />
+            </CardHeader>
+            <CardBody className="py-6">
+              <Typography variant="h3" color="blue-gray" className="ml-2 font-bold mb-2">
+                {card.title}
+              </Typography>
+              <Typography className='px-3'>
+                {card.text}
+              </Typography>
+            </CardBody>
+            <CardFooter className='flex justify-center'>
+              <button className='bg-blue-500 rounded-lg text-xl mb-3 text-white leading-none px-2 py-2' >Explore!</button>
+            </CardFooter> 
+          </Card>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 px-6 justify-center lg:px-40">
+        {cards2.map((card, index) => (
+          <Card key={index} className="mt-6">
+            <CardHeader color="blue-gray" className="relative h-56 rounded-lg">
+              <img
+                src={card.imageURL}
+                alt="card-image"
+                className="object-cover w-full h-full rounded-lg"
+              />
+            </CardHeader>
+            <CardBody className="py-6">
+              <Typography variant="h3" color="blue-gray" className="ml-2 font-bold mb-2">
+                {card.title}
+              </Typography>
+              <Typography className='px-3'>
+                {card.text}
+              </Typography>
+            </CardBody>
+            <CardFooter className='flex justify-center'>
+              <button className='bg-blue-500 rounded-lg text-xl mb-3 text-white leading-none px-2 py-2' >Explore!</button>
+            </CardFooter> 
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
+
+export default Cards;
