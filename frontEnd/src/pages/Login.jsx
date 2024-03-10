5093d7055c118f24775e1cdcfec53c83b49882a5
EccentricHarry <harinathrao13@gmail.com>
2024-03-10 21:44:27 +0530

    modifyy

--- frontEnd/src/pages/Login.jsx
+++ frontEnd/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import login from "../assets/icons/login.jpg";
+import loginBg from "../assets/icons/login.jpg";
+import logo from '../assets/icons/logo.png';
 
 const Login = () => {
   const [password, setPassword] = useState("");
@@ -14,7 +15,7 @@ const Login = () => {
         username,
         password,
       });
-      console.log(response.data); // Log the data here
+      console.log(response.data);
       if (response.status === 200) {
         localStorage.setItem("accessToken", response.data.accessToken);
         localStorage.setItem("username", response.data.username);
@@ -26,40 +27,47 @@ const Login = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center h-screen bg-cover bg-center bg-no-repeat bg-white" style={{backgroundImage: `url(${login})`}}>
-        <div className="w-3/4 lg:w-1/3 bg-white bg-opacity-80 shadow-lg rounded-lg p-8"> 
-          <h1 className="text-4xl font-bold text-center text-gray-800 mb-4">Login</h1> 
-          <form onSubmit={handleFormSubmit} className="space-y-4"> 
-            <div className="flex flex-col"> 
-              <label htmlFor="username" className="text-gray-600 text-sm font-medium">Username</label> 
-              <input
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Username"
-                required
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400" 
-              />
-            </div>
-            <div className="flex flex-col">
-              <label htmlFor="password" className="text-gray-600 text-sm font-medium">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                required
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-              />
-            </div>
-            <div className="flex justify-end"> 
-              <button type="submit" className="bg-blue-500 text-white font-bold px-6 py-2 rounded-md hover:bg-blue-600">Sign In</button> 
-            </div>
-          </form>
+      <div className="relative h-screen">
+        <img src={loginBg} alt="Background" className="absolute inset-0 w-full h-full object-cover" />
+        <div className="absolute top-0 left-0 p-4 flex items-center">
+          <img src={logo} alt="Better Care Logo" className="w-12 h-12 mr-2"/>
+          <h1 className="text-2xl font-bold text-gray-800">Better Care</h1>
+        </div>
+        <div className="flex justify-center items-center h-full">
+          <div className="max-w-lg bg-white bg-opacity-80 shadow-lg rounded-lg p-8 relative"> 
+            <h2 className="text-2xl font-bold text-center text-gray-800 mb-4">Login</h2> 
+            <form onSubmit={handleFormSubmit} className="space-y-4"> 
+              <div className="flex flex-col"> 
+                <label htmlFor="username" className="text-gray-600 text-sm font-medium">Username</label> 
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="Username"
+                  required
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400" 
+                />
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="password" className="text-gray-600 text-sm font-medium">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  required
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                />
+              </div>
+              <div className="flex justify-center"> 
+                <button type="submit" className="bg-blue-500 text-white font-bold px-6 py-2 rounded-md hover:bg-blue-600">Sign In</button> 
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
--- frontEnd/src/pages/SignUp.jsx
+++ frontEnd/src/pages/SignUp.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
-import signup from "../assets/icons/signup.png";
-import bg from "../assets/icons/signupbg.jpg";
-import logo from "../assets/icons/logo.png";
-import { Link } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+import signup from '../assets/icons/signup.png';
+import bg from '../assets/icons/signupbg.jpg';
+import logo from '../assets/icons/logo.png';
+
 const SignUp = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
-  let status = 0;
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -23,129 +21,89 @@ const SignUp = () => {
         username,
         password,
       });
-      status = response.status;
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
   };
-
+  
   return (
     <>
-      {status == 200 ? (
-        <Navigate to="/login" />
-      ) : (
-        <div
-          className="relative h-screen bg-cover bg-center bg-no-repeat"
-          style={{ backgroundImage: `url(${bg})` }}
-        >
-          <div className="absolute top-0 left-0 p-4 flex items-center">
-            <img src={logo} alt="Better Care Logo" className="w-12 h-12 mr-2" />
-            <h1 className="text-2xl font-bold text-gray-800">Better Care</h1>
-          </div>
-          <div className="flex justify-center items-center h-full">
-            <div className="flex w-3/4 lg:w-1/2 bg-white bg-opacity-80 shadow-lg rounded-lg p-8 relative">
-              <div className="w-1/2">
-                <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">
-                  Sign Up
-                </h1>
-                <form onSubmit={handleFormSubmit} className="space-y-4">
-                  <div className="flex flex-col">
-                    <label
-                      htmlFor="name"
-                      className="text-gray-600 text-sm font-medium"
-                    >
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      required
-                      placeholder="Name"
-                      onChange={(e) => {
-                        setName(e.target.value);
-                      }}
-                      className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label
-                      htmlFor="email"
-                      className="text-gray-600 text-sm font-medium"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      required
-                      placeholder="Email"
-                      onChange={(e) => {
-                        setEmail(e.target.value);
-                      }}
-                      className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label
-                      htmlFor="username"
-                      className="text-gray-600 text-sm font-medium"
-                    >
-                      Username
-                    </label>
-                    <input
-                      type="text"
-                      id="username"
-                      name="username"
-                      placeholder="Username"
-                      required
-                      onChange={(e) => {
-                        setUsername(e.target.value);
-                      }}
-                      className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label
-                      htmlFor="password"
-                      className="text-gray-600 text-sm font-medium"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      id="password"
-                      name="password"
-                      required
-                      placeholder="Password"
-                      onChange={(e) => setPassword(e.target.value)}
-                      className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    />
-                  </div>
-                  <div className="flex justify-center">
-                    <button
-                      type="submit"
-                      className="bg-blue-500 text-white font-bold px-6 py-2 rounded-md hover:bg-blue-600"
-                    >
-                      Sign Up
-                    </button>
-                  </div>
-                  <div>
-                    Already have an account{" "}
-                    <Link to="/login" className="text-teal-600 underline">
-                      Login
-                    </Link>
-                  </div>
-                </form>
-              </div>
-              <div className="w-1/2 flex justify-center items-center">
-                <img src={signup} alt="signup icon" className="w-64 h-64" />
-              </div>
+      <div className="relative h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${bg})`}}>
+        <div className="absolute top-0 left-0 p-4 flex items-center">
+          <img src={logo} alt="Better Care Logo" className="w-12 h-12 mr-2"/>
+          <h1 className="text-2xl font-bold text-gray-800">Better Care</h1>
+        </div>
+        <div className="flex justify-center items-center h-full">
+          <div className="flex w-3/4 lg:w-1/2 bg-white bg-opacity-80 shadow-lg rounded-lg p-8 relative"> 
+            <div className="w-1/2">
+              <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">Sign Up</h1> 
+              <form onSubmit={handleFormSubmit} className="space-y-4"> 
+                <div className="flex flex-col">
+                  <label htmlFor="name" className="text-gray-600 text-sm font-medium">Name</label> 
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    required
+                    placeholder="Name"
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                    className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400" 
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="email" className="text-gray-600 text-sm font-medium">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    required
+                    placeholder="Email"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="username" className="text-gray-600 text-sm font-medium">Username</label>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Username"
+                    required
+                    onChange={(e) => {
+                      setUsername(e.target.value);
+                    }}
+                    className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="password" className="text-gray-600 text-sm font-medium">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                  />
+                </div>
+                <div className="flex justify-center">
+                  <button type="submit" className="bg-blue-500 text-white font-bold px-6 py-2 rounded-md hover:bg-blue-600">Sign Up</button> 
+                </div>
+              </form>
+            </div>
+            <div className="w-1/2 flex justify-center items-center">
+              <img src={signup} alt="signup icon" className="w-64 h-64"/> 
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
